2e1900f653fcc129e20ed8096887b713d2fef972
Cruz Ortiz <cruzortiz099@gmail.com>
2019-07-18 16:40:48 -0400

    structure done

--- client/src/views/Introduction.tsx
+++ client/src/views/Introduction.tsx
@@ -3,6 +3,20 @@ import AppContentStructure from '../components/AppContentStructure'
 
 const Introduction = () => {
   let title = 'Introducción'
-  return <AppContentStructure title={title} />
+  let paragraphs = (
+    <React.Fragment>
+      Esto es el curso para principiantes en JavaScript
+    </React.Fragment>
+  )
+  let example = <React.Fragment>Example</React.Fragment>
+  let code = <React.Fragment>Code</React.Fragment>
+  return (
+    <AppContentStructure
+      title={title}
+      paragraphs={paragraphs}
+      example={example}
+      code={code}
+    />
+  )
 }
 export default Introduction
