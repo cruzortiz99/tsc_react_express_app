440795f95f4a2408c567abf88776723d2ee51d74
Cruz Ortiz <cruzortiz099@gmail.com>
2019-07-19 14:10:00 -0400

    improving component

--- client/src/components/AppMenuClassification.tsx
+++ client/src/components/AppMenuClassification.tsx
@@ -3,7 +3,7 @@ import AppMenuItem, { AppMenuItemProps } from './AppMenuItem'
 import AppHeader, { AppHeaderProps } from './AppHeader'
 
 export interface AppMenuClassificationProps {
-  name: string
+  name?: string
   children: Array<AppMenuItemProps>
 }
 
@@ -11,12 +11,20 @@ const AppMenuClassification = (props: AppMenuClassificationProps) => {
   let items = props.children.map((item, key) => {
     return <AppMenuItem link={item.link} name={item.name} key={key} />
   })
-  return (
-    <React.Fragment>
-      <AppHeader text={props.name} level={3} />
-      <ul>{items}</ul>
-    </React.Fragment>
-  )
+  if (props.name) {
+    return (
+      <React.Fragment>
+        <AppHeader text={props.name} level={3} />
+        <ul>{items}</ul>
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <ul>{items}</ul>
+      </React.Fragment>
+    )
+  }
 }
 
 export default AppMenuClassification
