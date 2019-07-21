d2016de38686a1ccf6c451ef240868e05f5855dd
Cruz Ortiz <cortiz99>
2019-07-20 20:11:22 -0400

    adding content to the index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -15,14 +15,125 @@ class App extends React.Component<{}, AppState> {
       menu: {
         options: [
           {
+            name: 'básico',
             children: [
               {
                 link: '/introduction',
-                name: 'Introduction'
+                name: 'introduction'
               },
               {
                 link: '/syntax',
-                name: 'Syntax'
+                name: 'syntax'
+              },
+              {
+                link: '/comments',
+                name: 'comments'
+              },
+              {
+                link: '/variables',
+                name: 'variables'
+              },
+              {
+                link: '/variables/scopes',
+                name: 'scopes'
+              },
+              {
+                link: '/variables/hoisting',
+                name: 'hoisting'
+              },
+              {
+                link: '/statements',
+                name: 'statements'
+              },
+              {
+                link: '/operations',
+                name: 'operations'
+              },
+              {
+                link: '/functions',
+                name: 'functions'
+              },
+              {
+                link: '/functions/arrow-functions',
+                name: 'arrow functions'
+              },
+              {
+                link: '/conditions',
+                name: 'conditions'
+              },
+              {
+                link: '/loops',
+                name: 'loops'
+              }
+            ]
+          },
+          {
+            name: 'intermedio',
+            children: [
+              {
+                link: '/events',
+                name: 'events'
+              },
+              {
+                link: '/math',
+                name: 'math'
+              },
+              {
+                link: '/arrays',
+                name: 'arrays'
+              },
+              {
+                link: '/dates',
+                name: 'dates'
+              },
+              {
+                link: '/strings',
+                name: 'strings'
+              },
+              {
+                link: '/dom-access',
+                name: 'dom access'
+              },
+              {
+                link: '/json',
+                name: 'json'
+              },
+              {
+                link: '/prototypes',
+                name: 'prototypes'
+              },
+              {
+                link: '/classes',
+                name: 'classes'
+              }
+            ]
+          },
+          {
+            name: 'avanzado',
+            children: [
+              {
+                link: '/reactjs',
+                name: 'reactjs'
+              },
+              {
+                link: '/vuejs',
+                name: 'vuejs'
+              },
+              {
+                link: '/angularjs',
+                name: 'angularjs'
+              },
+              {
+                link: '/nodejs',
+                name: 'nodejs'
+              },
+              {
+                link: '/typescript',
+                name: 'typescript'
+              },
+              {
+                link: 'flow',
+                name: 'flow'
               }
             ]
           }
