ba1444922f89d94951b3b2c62fa1d5cf215247fe
Cruz Ortiz <cortiz99>
2019-07-20 21:03:32 -0400

    using react hook to build funtional components

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useState } from 'react'
 import AppMenu, { AppMenuProps } from './components/AppMenu/AppMenu'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Introduction from './views/Introduction'
@@ -8,150 +9,140 @@ import './css/main.css'
 interface AppState {
   menu: AppMenuProps
 }
-class App extends React.Component<{}, AppState> {
-  constructor(props = {}) {
-    super(props)
-    this.state = {
-      menu: {
-        options: [
-          {
-            name: 'básico',
-            children: [
-              {
-                link: '/introduction',
-                name: 'introduction'
-              },
-              {
-                link: '/syntax',
-                name: 'syntax'
-              },
-              {
-                link: '/comments',
-                name: 'comments'
-              },
-              {
-                link: '/variables',
-                name: 'variables'
-              },
-              {
-                link: '/variables/scopes',
-                name: 'scopes'
-              },
-              {
-                link: '/variables/hoisting',
-                name: 'hoisting'
-              },
-              {
-                link: '/statements',
-                name: 'statements'
-              },
-              {
-                link: '/operations',
-                name: 'operations'
-              },
-              {
-                link: '/functions',
-                name: 'functions'
-              },
-              {
-                link: '/functions/arrow-functions',
-                name: 'arrow functions'
-              },
-              {
-                link: '/conditions',
-                name: 'conditions'
-              },
-              {
-                link: '/loops',
-                name: 'loops'
-              }
-            ]
-          },
-          {
-            name: 'intermedio',
-            children: [
-              {
-                link: '/events',
-                name: 'events'
-              },
-              {
-                link: '/math',
-                name: 'math'
-              },
-              {
-                link: '/arrays',
-                name: 'arrays'
-              },
-              {
-                link: '/dates',
-                name: 'dates'
-              },
-              {
-                link: '/strings',
-                name: 'strings'
-              },
-              {
-                link: '/dom-access',
-                name: 'dom access'
-              },
-              {
-                link: '/json',
-                name: 'json'
-              },
-              {
-                link: '/prototypes',
-                name: 'prototypes'
-              },
-              {
-                link: '/classes',
-                name: 'classes'
-              }
-            ]
-          },
-          {
-            name: 'avanzado',
-            children: [
-              {
-                link: '/reactjs',
-                name: 'reactjs'
-              },
-              {
-                link: '/vuejs',
-                name: 'vuejs'
-              },
-              {
-                link: '/angularjs',
-                name: 'angularjs'
-              },
-              {
-                link: '/nodejs',
-                name: 'nodejs'
-              },
-              {
-                link: '/typescript',
-                name: 'typescript'
-              },
-              {
-                link: 'flow',
-                name: 'flow'
-              }
-            ]
-          }
-        ]
-      }
+const App = () => {
+  let [options, setOptions] = useState([
+    {
+      name: 'básico',
+      children: [
+        {
+          link: '/introduction',
+          name: 'introduction'
+        },
+        {
+          link: '/syntax',
+          name: 'syntax'
+        },
+        {
+          link: '/comments',
+          name: 'comments'
+        },
+        {
+          link: '/variables',
+          name: 'variables'
+        },
+        {
+          link: '/variables/scopes',
+          name: 'scopes'
+        },
+        {
+          link: '/variables/hoisting',
+          name: 'hoisting'
+        },
+        {
+          link: '/statements',
+          name: 'statements'
+        },
+        {
+          link: '/operations',
+          name: 'operations'
+        },
+        {
+          link: '/functions',
+          name: 'functions'
+        },
+        {
+          link: '/functions/arrow-functions',
+          name: 'arrow functions'
+        },
+        {
+          link: '/conditions',
+          name: 'conditions'
+        },
+        {
+          link: '/loops',
+          name: 'loops'
+        }
+      ]
+    },
+    {
+      name: 'intermedio',
+      children: [
+        {
+          link: '/events',
+          name: 'events'
+        },
+        {
+          link: '/math',
+          name: 'math'
+        },
+        {
+          link: '/arrays',
+          name: 'arrays'
+        },
+        {
+          link: '/dates',
+          name: 'dates'
+        },
+        {
+          link: '/strings',
+          name: 'strings'
+        },
+        {
+          link: '/dom-access',
+          name: 'dom access'
+        },
+        {
+          link: '/json',
+          name: 'json'
+        },
+        {
+          link: '/prototypes',
+          name: 'prototypes'
+        },
+        {
+          link: '/classes',
+          name: 'classes'
+        }
+      ]
+    },
+    {
+      name: 'avanzado',
+      children: [
+        {
+          link: '/reactjs',
+          name: 'reactjs'
+        },
+        {
+          link: '/vuejs',
+          name: 'vuejs'
+        },
+        {
+          link: '/angularjs',
+          name: 'angularjs'
+        },
+        {
+          link: '/nodejs',
+          name: 'nodejs'
+        },
+        {
+          link: '/typescript',
+          name: 'typescript'
+        },
+        {
+          link: 'flow',
+          name: 'flow'
+        }
+      ]
     }
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <AppMenu options={this.state.menu.options} />
-          <Route exact path="/(introduction)*" component={Introduction} />
-        </Router>
-      </React.Fragment>
-    )
-  }
+  ])
+  return (
+    <React.Fragment>
+      <Router>
+        <AppMenu options={options} />
+        <Route exact path="/(introduction)*" component={Introduction} />
+      </Router>
+    </React.Fragment>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
