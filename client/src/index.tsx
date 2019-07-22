ecc46f01aa2a8977f53577a39340589868bbee5d
Cruz Ortiz <cortiz99>
2019-07-21 20:24:36 -0400

    cleaning up

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { useState } from 'react'
-import AppMenu, { AppMenuProps } from './components/AppMenu/AppMenu'
+import AppMenu from './components/AppMenu/AppMenu'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Introduction from './views/Introduction'
 import './css/main.css'
 import menu from './content/menu.json'
 
-interface AppState {
-  menu: AppMenuProps
-}
 const App = () => {
   /* Using react hook to set the state of a functional component */
   let [options] = useState(menu)
