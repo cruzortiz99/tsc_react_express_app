8a78fa3c9bb85afd7ded03329ee25963f003426d
Cruz Ortiz <cruzortiz099@gmail.com>
2019-07-22 16:58:17 -0400

    active route class

--- client/src/components/AppMenuItem/AppMenuItem.tsx
+++ client/src/components/AppMenuItem/AppMenuItem.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, NavLink, match } from 'react-router-dom'
 import './app-menu-item.css'
 
 export interface AppMenuItemProps {
@@ -8,10 +8,21 @@ export interface AppMenuItemProps {
 }
 
 const AppMenuItem = (props: AppMenuItemProps) => {
+  let [className, setClassName] = useState('app-menu-item app-menu-item-hover')
   useEffect(() => {}, [false])
+  let isActive = (match: any, location: any) => {
+    if (match) {
+      setClassName('app-menu-item app-menu-clicked-link')
+      return true
+    }
+    setClassName('app-menu-item app-menu-item-hover')
+    return match
+  }
   return (
-    <li className="app-menu-item">
-      <Link to={props.link}>{props.name}</Link>
+    <li className={className}>
+      <NavLink to={props.link} isActive={isActive} exact>
+        {props.name}
+      </NavLink>
     </li>
   )
 }
