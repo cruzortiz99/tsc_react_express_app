416da9b4c02868bc51680ff94ce425c719706001
Cruz Ortiz <cruzortiz099@gmail.com>
2019-07-23 09:08:20 -0400

    menu style done

--- client/src/components/AppHeader/AppHeader.tsx
+++ client/src/components/AppHeader/AppHeader.tsx
@@ -5,19 +5,14 @@ export interface AppHeaderProps {
   level?: number
 }
 const AppHeader = (props: AppHeaderProps) => {
-  switch (props.level) {
-    case 1:
-      return <h2 className="app-header-uppercase">{props.text}</h2>
-    case 2:
-      return <h3 className="app-header-capitalize">{props.text}</h3>
-    case 3:
-      return <h4 className="app-header-capitalize">{props.text}</h4>
-    case 5:
-      return <h5 className="app-header-capitalize">{props.text}</h5>
-    case 6:
-      return <h6 className="app-header-capitalize">{props.text}</h6>
-    default:
-      return <h1 className="app-header-uppercase">{props.text}</h1>
-  }
+  let level = props.level || 0
+  let className = level < 3 ? 'app-header-uppercase' : 'app-header-capitalize'
+  return React.createElement(
+    `h${level + 1}`,
+    {
+      className
+    },
+    props.text
+  )
 }
 export default AppHeader
