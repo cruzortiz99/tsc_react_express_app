d166d1e8e9f8c6b27363f6d341e825ba7c31ef43
Cruz Ortiz <cruzortiz099@gmail.com>
2019-08-13 11:31:00 -0400

    if else class done

--- client/src/pages/Basics/Conditions.tsx
+++ client/src/pages/Basics/Conditions.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
 import { AppPagesProps } from '../AppPages'
 import AppContentStructure from '../../components/AppContentStructure/AppContentStructure'
+import AppContentTheory from '../../components/AppContentTheory/AppContentTheory'
 
 export default (props: AppPagesProps) => (
-  <AppContentStructure title='Condicionales' {...props}></AppContentStructure>
+  <AppContentStructure title='Condicionales' {...props}>
+    <AppContentTheory>
+      <p>
+        Un proceso natural de cualquier tipo posee dos secciones
+        características, las bifurcaciones y las iteraciones
+      </p>
+      <ul>
+        <li>
+          Las bifurcaciones, no son más que caminos que puede tomar o acciones
+          que pueden ejecutarse en la aplicación dada una condición.
+        </li>
+        <li>
+          Las iteraciones por otra parte, son flujos que se repiten una cantidad
+          de veces hasta llegar a un estado en particular
+        </li>
+      </ul>
+      <p>
+        Por ejemplo, si se necesita que un elemento en la interfaz de la
+        aplicación se vea o no, se utilizaría un condicional para que la
+        aplicación entienda, que existen varios caminos a partir de un estado.
+        Por otra parte, que pasa si necesitamos, por ejemplo, implementar algún
+        tipo de contador o de cálculo arduo que amerite repetir una sección de
+        código una cantidad de veces, bueno para este escenario necesitamos de
+        los iteradores
+      </p>
+      <ul>
+        Condicionales
+        <li>If-else</li>
+        <li>Switch-Case</li>
+      </ul>
+      <ul>
+        Iteradores
+        <li>For</li>
+        <li>While</li>
+      </ul>
+    </AppContentTheory>
+  </AppContentStructure>
 )
--- client/src/pages/Basics/IfElses.tsx
+++ client/src/pages/Basics/IfElses.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 import { AppPagesProps } from '../AppPages'
 import AppContentStructure from '../../components/AppContentStructure/AppContentStructure'
+import AppContentTheory from '../../components/AppContentTheory/AppContentTheory'
+import AppContentCode from '../../components/AppContentCode/AppContentCode'
 
 export default (props: AppPagesProps) => (
-  <AppContentStructure title='If-Else' {...props} />
+  <AppContentStructure title='If-Else' {...props}>
+    <AppContentTheory>
+      <p>
+        Permite crear diferentes caminos en la aplicación a partir de una
+        condición, la cantidad de caminos es siempre dos. Si la condición se
+        cumple o no. Para crear más caminos, se van uniendo a los caminos nuevas
+        condiciones
+      </p>
+    </AppContentTheory>
+    <AppContentCode type='js'>
+      {
+        '// Dos caminos\nvar number = 2;\nif (number >= 2) {\n console.log("Numero es mayor o igual a dos");\n} else {\n console.log("Numero es menor a dos");\n}'
+      }
+    </AppContentCode>
+    <AppContentCode type='js'>
+      {
+        '// Mas de dos caminos\nvar number = 2;\nif (number > 2) {\n console.log("Numero es mayor a dos");\n} else if (number === 2) {\n console.log("Numero es igual a dos");\n} else {\n console.log("Numero es menor que dos");\n}'
+      }
+    </AppContentCode>
+  </AppContentStructure>
 )
