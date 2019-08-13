42aa01d81cdf26e1a88c8a8ff8b499922573447c
Cruz Ortiz <cruzortiz099@gmail.com>
2019-08-13 13:54:57 -0400

    ciclos listo

--- client/src/pages/Basics/Loops.tsx
+++ client/src/pages/Basics/Loops.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import { AppPagesProps } from '../AppPages'
 import AppContentStructure from '../../components/AppContentStructure/AppContentStructure'
+import AppContentTheory from '../../components/AppContentTheory/AppContentTheory'
 
 export default (props: AppPagesProps) => (
-  <AppContentStructure title='Ciclos' {...props} />
+  <AppContentStructure title='Ciclos' {...props}>
+    <AppContentTheory>
+      <p>
+        Los ciclos son importantes al momento de realizar una aplicación,
+        permiten repetir procesos comunes una cantidad de veces específica o
+        dependiendo de una condición. Los ciclos también son muy útiles para
+        recorrer arreglos o matrices
+      </p>
+      <p>
+        Dependiendo del lenguaje existen maneras de realizar los ciclos, en
+        JavaScript tenemos las siguientes
+      </p>
+      <ul>
+        <li>For</li>
+        <li>While</li>
+        <li>Funciones recursivas</li>
+      </ul>
+    </AppContentTheory>
+  </AppContentStructure>
 )
