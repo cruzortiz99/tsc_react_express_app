b89a8eebfb9abb3be475f0479e4e14282c75130f
Cruz Ortiz <cruzortiz099@gmail.com>
2019-09-02 08:36:53 -0400

    better practices applyed

--- server/client/src/pages/Basics/Basics.tsx
+++ server/client/src/pages/Basics/Basics.tsx
@@ -8,62 +8,7 @@ import { AppPagesProps } from '../AppPages'
 const BasicsPage = (props: AppPagesProps) => {
   const [displayHolaMundo, setDisplayHolaMundo] = useState('none')
   const title = 'Introducción'
-  const paragraphs = (
-    <AppContentTheory>
-      <p>
-        JavaScript es el lenguaje de de la Web, es lo que permite a las paginas
-        webs pasar de ser estáticas a ser páginas dinámicas o aplicaciones webs.
-        Ofrece muchas ventajas aprender javascript:
-      </p>
-      <ul>
-        <li>Fácil de aprender</li>
-        <li>
-          Sigue evolucionando, añadiendo funcionalidades y mejorando la
-          experiencia del desarrollador
-        </li>
-        <li>
-          Es el lenguaje que utiliza la web, si quieres desarrollar web
-          necesitas JS
-        </li>
-        <li>La comunidad tiene tendencia open source</li>
-        <li>La cantidad de librerías y frameworks crece</li>
-        <li>Posee una gran comunidad que se expande</li>
-      </ul>
-    </AppContentTheory>
-  )
-  const example = (
-    <AppContentExample boxName='Ejemplo'>
-      <div>
-        <p style={{ display: displayHolaMundo }}>Hola Mundo</p>
-        <button
-          onClick={() => {
-            setDisplayHolaMundo(prevState => {
-              return prevState === 'none' ? 'block' : 'none'
-            })
-          }}
-        >
-          {displayHolaMundo === 'none' ? 'Mostrar' : 'Ocultar'}
-        </button>
-      </div>
-    </AppContentExample>
-  )
-  const codeJs = (
-    <AppContentCode type='js'>
-      {`function showParagraph () {
-    var paragraph = document.getElementById('paragraph')
-    var currentDisplayValue =  paragraph.style.display
-    paragraph.style.display = (currentDisplayValue === 'none') ? 'block' : 'none'
-    var button = document.getElementById('button')
-    console.log(currentDisplayValue)
-    button.innerText = (currentDisplayValue !== 'none') ? 'Mostrar' :  'Ocultar'\n}`}
-    </AppContentCode>
-  )
-  const codeHtml = (
-    <AppContentCode type='html'>
-      {`<p id="paragraph" style="{display: 'none'}"> Hola Mundo </p>'
-<button id="button" on-click="showParagraph()"> Mostrar </button>`}
-    </AppContentCode>
-  )
+
   return (
     <AppContentStructure
       title={title}
@@ -71,10 +16,54 @@ const BasicsPage = (props: AppPagesProps) => {
       nextRoute={props.nextRoute}
       {...props}
     >
-      {example}
-      {paragraphs}
-      {codeHtml}
-      {codeJs}
+      <AppContentExample boxName='Ejemplo'>
+        <div>
+          <p style={{ display: displayHolaMundo }}>Hola Mundo</p>
+          <button
+            onClick={() => {
+              setDisplayHolaMundo(prevState => {
+                return prevState === 'none' ? 'block' : 'none'
+              })
+            }}
+          >
+            {displayHolaMundo === 'none' ? 'Mostrar' : 'Ocultar'}
+          </button>
+        </div>
+      </AppContentExample>
+      <AppContentTheory>
+        <p>
+          JavaScript es el lenguaje de de la Web, es lo que permite a las
+          paginas webs pasar de ser estáticas a ser páginas dinámicas o
+          aplicaciones webs. Ofrece muchas ventajas aprender javascript:
+        </p>
+        <ul>
+          <li>Fácil de aprender</li>
+          <li>
+            Sigue evolucionando, añadiendo funcionalidades y mejorando la
+            experiencia del desarrollador
+          </li>
+          <li>
+            Es el lenguaje que utiliza la web, si quieres desarrollar web
+            necesitas JS
+          </li>
+          <li>La comunidad tiene tendencia open source</li>
+          <li>La cantidad de librerías y frameworks crece</li>
+          <li>Posee una gran comunidad que se expande</li>
+        </ul>
+      </AppContentTheory>
+      <AppContentCode type='html'>
+        {`<p id="paragraph" style="{display: 'none'}"> Hola Mundo </p>'
+<button id="button" on-click="showParagraph()"> Mostrar </button>`}
+      </AppContentCode>
+      <AppContentCode type='js'>
+        {`function showParagraph () {
+    var paragraph = document.getElementById('paragraph')
+    var currentDisplayValue =  paragraph.style.display
+    paragraph.style.display = (currentDisplayValue === 'none') ? 'block' : 'none'
+    var button = document.getElementById('button')
+    console.log(currentDisplayValue)
+    button.innerText = (currentDisplayValue !== 'none') ? 'Mostrar' :  'Ocultar'\n}`}
+      </AppContentCode>
     </AppContentStructure>
   )
 }
