ab76147c6820710686ce8919c91dd1d25b11b7c8
Cruz Ortiz <cruzortiz099@gmail.com>
2019-09-02 12:03:50 -0400

    atom loader customizable

--- server/client/src/components/AppLoader/AppLoader.tsx
+++ server/client/src/components/AppLoader/AppLoader.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
 import classes from './app-loader.css'
-
-const AppLoader = () => {
-  return <div className={classes.loader}></div>
+export interface AppLoaderProps {
+  coreColor?: string
+  electronXColor?: string
+  electronYColor?: string
+  electronZColor?: string
+}
+const AppLoader = (props: AppLoaderProps) => {
+  const style = {
+    core: {
+      backgroundColor: props.coreColor || 'gray'
+    },
+    electronX: {
+      borderTopColor: props.electronXColor || 'gray'
+    },
+    electronY: {
+      borderTopColor: props.electronYColor || 'gray'
+    },
+    electronZ: {
+      borderTopColor: props.electronZColor || 'gray'
+    }
+  }
+  return (
+    <div className={classes.loader}>
+      <div className={classes.core} style={style.core}>
+        <div
+          className={[classes.electron, classes.rotateX].join(' ')}
+          style={style.electronX}
+        ></div>
+        <div
+          className={[classes.electron, classes.rotateY].join(' ')}
+          style={style.electronY}
+        ></div>
+        <div
+          className={[classes.electron, classes.rotateZ].join(' ')}
+          style={style.electronZ}
+        ></div>
+      </div>
+    </div>
+  )
 }
 export default AppLoader
